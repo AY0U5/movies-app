043371226267624b842e585f2689aa47ad2cb86c
ayoub <mahdaoui.ayoub35@gmail.com>
2024-02-10 21:06:51 +0100

    update

--- src/main/java/ayb/app/movieapp/WS/facade/MovieWS.java
+++ src/main/java/ayb/app/movieapp/WS/facade/MovieWS.java
@@ -5,14 +5,12 @@ import ayb.app.movieapp.WS.dto.MovieDto;
 import ayb.app.movieapp.bean.Movie;
 import ayb.app.movieapp.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/movie/")
 public class MovieWS {
     @Autowired
